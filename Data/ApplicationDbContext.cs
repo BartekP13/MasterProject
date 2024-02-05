e9d9219c8370166ce234c6349878c6760f956e91
Bartek <barpro3@st.amu.edu.pl>
2024-02-05 15:48:33 +0100

    first

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MasterProject.Models;
+
+namespace MasterProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MasterProject.Models.Recipe> Recipe { get; set; } = default!;
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿namespace MasterProject.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Kcal { get; set; }
+        public Recipe()
+        {
+
+        }
+    }
+}
+
